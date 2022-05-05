2081974bc7bdcc7edf9d865d838e495c7fa54ce2
maxim <makkov2705@gmail.com>
2022-05-05 15:04:06 +0300

    (feature) Add time function

--- server.go
+++ server.go
@@ -1,10 +1,21 @@
 package main
-
 import (
   "net/http"
   "time"
   "encoding/json"
-)
-
+) 
+type Time struct {
+  NowTime string `json:"time"`
+} 
 func main() {
+  const RFC3339 = "2006-01-02T15:04:05Z07:00" 
+  http.HandleFunc("/time", func(rw http.ResponseWriter, r *http.Request) {
+    now := time.Now()
+    formattedTime := now.Format(RFC3339) 
+    structTime := Time{formattedTime} 
+    data, _ := json.MarshalIndent(structTime, "", "\t")
+    rw.Write(data)
+  })
+  http.ListenAndServe(":8795", nil)
 }
+
